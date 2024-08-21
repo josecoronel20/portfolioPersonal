00cd9edd0957b33e9d80eed00a6b9dd197f99045
josecoronel20 <josecoronel20@outlook.com>
2024-08-21 21:13:52 +0100

    agrega componente modal exitoso

--- src/components/globalComponents/Nav.jsx
+++ src/components/globalComponents/Nav.jsx
@@ -3,20 +3,14 @@ import { Link } from "react-router-dom";
 import es from "../../utilities/lenguajes/es.json";
 import { iconMenu } from "../../utilities/Icons";
 import { useToggle } from "../customHooks/useToggle";
-import useClickTarget from "../customHooks/useClickTarget";
+import Modal from "./Modal";
 
 const Nav = () => {
   // Estado para obtener el tamaño de la pantalla y renderizar/aplicar estilos dependiendo de este
   const [windowSize, setWindowSize] = useState(window.innerWidth);
 
   // Uso de custom hook para manejar el estado toggle
-  const { isToggleOpen, handlerToggle } = useToggle();
-
-  //uso de custom hook para manejar el modal
-  const { elementRef } = useClickTarget({
-    toggle: isToggleOpen,
-    setToggle: handlerToggle,
-  });
+  const { isToggleOpen, handlerToggle, setIsToggleOpen } = useToggle();
 
   // Lógica para manejar el tamaño de pantalla
   const handlerWindowSize = () => {
@@ -33,30 +27,32 @@ const Nav = () => {
   }, [isToggleOpen, handlerToggle]);
 
   return (
-    <nav ref={elementRef}>
-      <div onClick={handlerToggle} className="relative z-20">
-        {windowSize < 768 && iconMenu}
-      </div>
+    <Modal toggle={isToggleOpen} setToggle={setIsToggleOpen}>
+      <nav>
+        <div onClick={handlerToggle} className="relative z-30">
+          {windowSize < 768 && iconMenu}
+        </div>
 
-      <ul
-        className={`${
-          isToggleOpen === false && "hidden"
-        } fixed bg-darkLight top-0 right-0 h-full w-2/3 flex flex-col justify-center gap-5 text-center text-lightLight`}
-      >
-        <li>
-          <Link to="/">{es.nav.home}</Link>
-        </li>
-        <li>
-          <Link to="/projects">{es.nav.projects}</Link>
-        </li>
-        <li>
-          <Link to="/blog">{es.nav.blog}</Link>
-        </li>
-        <li>
-          <Link to="/contact">{es.nav.contact}</Link>
-        </li>
-      </ul>
-    </nav>
+        <ul
+          className={`${
+            isToggleOpen === false && "hidden"
+          } fixed z-20 bg-darkLight top-0 right-0 h-full w-2/3 flex flex-col justify-center gap-5 text-center text-lightLight`}
+        >
+          <li>
+            <Link to="/">{es.nav.home}</Link>
+          </li>
+          <li>
+            <Link to="/projects">{es.nav.projects}</Link>
+          </li>
+          <li>
+            <Link to="/blog">{es.nav.blog}</Link>
+          </li>
+          <li>
+            <Link to="/contact">{es.nav.contact}</Link>
+          </li>
+        </ul>
+      </nav>
+    </Modal>
   );
 };
 
